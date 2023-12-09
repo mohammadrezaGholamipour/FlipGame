d15d002afaf60a36dd5df84066ce0c647ccf672f
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2023-12-09 07:02:53 +0330

    fix timer

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,27 +1,82 @@
 <script setup>
-import { reactive } from "vue";
+import { onMounted, reactive } from "vue";
 /////////////////////////
+let sound = new Audio("/assets/alarm.wav");
 const state = reactive({
-  list: [
-    { id: 1, showImage: false },
-    { id: 2, showImage: false },
-    { id: 3, showImage: false },
-    { id: 4, showImage: false },
-    { id: 5, showImage: false },
-    { id: 6, showImage: false },
-    { id: 7, showImage: false },
-    { id: 8, showImage: false },
-    { id: 2, showImage: false },
-    { id: 4, showImage: false },
-    { id: 7, showImage: false },
-    { id: 1, showImage: false },
-    { id: 8, showImage: false },
-    { id: 3, showImage: false },
-    { id: 4, showImage: false },
-    { id: 5, showImage: false },
-  ],
+  list: [],
+  soundStatus: true,
+  numberOfMoves: 40,
+  timer: { minute: 2, second: 0, setInterval: 0 },
+  startGame: false,
 });
-//////////////////////
+
+onMounted(() => handleShuffleAndDuplicate());
+
+const handleShuffleAndDuplicate = () => {
+  const array = [
+    { id: 1, showImage: false, imageName: 1, found: false },
+    { id: 2, showImage: false, imageName: 1, found: false },
+    { id: 3, showImage: false, imageName: 2, found: false },
+    { id: 4, showImage: false, imageName: 2, found: false },
+    { id: 5, showImage: false, imageName: 3, found: false },
+    { id: 6, showImage: false, imageName: 3, found: false },
+    { id: 7, showImage: false, imageName: 4, found: false },
+    { id: 8, showImage: false, imageName: 4, found: false },
+    { id: 9, showImage: false, imageName: 5, found: false },
+    { id: 10, showImage: false, imageName: 5, found: false },
+    { id: 11, showImage: false, imageName: 6, found: false },
+    { id: 12, showImage: false, imageName: 6, found: false },
+    { id: 13, showImage: false, imageName: 7, found: false },
+    { id: 14, showImage: false, imageName: 7, found: false },
+    { id: 15, showImage: false, imageName: 8, found: false },
+    { id: 16, showImage: false, imageName: 8, found: false },
+  ];
+  // Duplicate each element
+  const duplicatedArray = array.map((item) => item);
+
+  // Shuffle the array
+  const shuffledArray = duplicatedArray.sort(() => Math.random() - 0.5);
+
+  state.list = shuffledArray;
+};
+
+const handleShowImage = (item) => {
+  if (!state.startGame) (state.startGame = true), handleTimer();
+  if (!item.showImage) state.numberOfMoves--;
+  item.showImage = !item.showImage;
+
+  const filterList = state.list.filter(
+    (filter) => filter.showImage && !filter.found
+  );
+
+  if (filterList.length === 2) {
+    if (filterList[0].imageName === filterList[1].imageName) {
+      filterList[0].found = true;
+      filterList[1].found = true;
+    } else {
+      setTimeout(() => {
+        state.list.forEach((changeShowImage) => {
+          changeShowImage.showImage = false;
+        });
+      }, 2000);
+    }
+  }
+};
+
+const handleTimer = () => {
+  state.timer.setInterval = setInterval(() => {
+    state.timer.second--;
+
+    if (state.timer.second <= 0) {
+      if (state.timer.minute) {
+        state.timer.minute--;
+        state.timer.second = 59;
+      } else {
+        clearInterval(state.timer.setInterval);
+      }
+    }
+  }, 1000);
+};
 </script>
 <template>
   <div class="parent-game">
@@ -29,29 +84,49 @@ const state = reactive({
       <div class="header">
         <div>
           <p>تعداد حرکت :</p>
-          <p>40</p>
+          <p>{{ state.numberOfMoves }}</p>
         </div>
         <div>
           <p>زمان :</p>
-          <p>2:00</p>
+          <p>{{ `${state.timer.minute}:${state.timer.second}` }}</p>
         </div>
       </div>
 
       <div class="main">
         <div
-          :class="item.showImage ? 'bg-white box-rotate ' : 'bg-[#DC3755]'"
-          @click="item.showImage = !item.showImage"
+          :class="
+            item.showImage || item.found
+              ? 'bg-white box-rotate '
+              : 'bg-[#DC3755]'
+          "
           v-for="(item, index) in state.list"
+          @click="handleShowImage(item)"
           :key="item.id"
           class="box"
         >
-          <transition-fade delay="100" group>
+          <transition-fade :delay="100" group>
             <img
-              :src="`src/assets/image/1/${item.id}.SVG`"
-              v-if="item.showImage"
+              :src="`src/assets/image/1/${item.imageName}.SVG`"
+              v-if="item.showImage || item.found"
             />
             <p v-else>{{ index + 1 }}</p>
           </transition-fade>
+        </div>
+      </div>
+
+      <div class="footer">
+        <button class="btn-start">
+          <p>شروع</p>
+          <img src="@/assets/image/start.svg" />
+        </button>
+
+        <div @click="state.soundStatus = !state.soundStatus" class="sound">
+          <img
+            :src="`src/assets/image/${
+              state.soundStatus ? 'sound-active' : 'sound-disable'
+            }.svg`"
+          />
+          <p>{{ state.soundStatus ? "فعال" : "غیر فعال" }}</p>
         </div>
       </div>
     </div>
@@ -90,5 +165,20 @@ const state = reactive({
 }
 .box-rotate {
   transform: rotateX(180deg);
+}
+.footer {
+  @apply w-full flex items-center justify-between;
+}
+.btn-start {
+  @apply flex gap-x-[10px] items-center px-[40px] py-[10px] rounded-[10px] transition-all bg-[#32BB71] hover:bg-[#47a374];
+}
+.btn-start p {
+  @apply text-white text-[18px] font-bold;
+}
+.sound {
+  @apply flex items-center gap-x-[10px] cursor-pointer;
+}
+.sound p {
+  @apply text-[18px] font-bold;
 }
 </style>
